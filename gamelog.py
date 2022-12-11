4126153e30a7fe7e6100ff35ed9af5b8c62c69e8
sallareznov <salla.diagne@meltwater.com>
2022-12-11 13:11:23 +0100

    try to fix timeout error

--- gamelog.py
+++ gamelog.py
@@ -9,7 +9,18 @@ from teams import Team
 
 
 def compute_gamelog(player: Player) -> Gamelog:
-    gamelog = PlayerGameLog(player.id).get_dict()["resultSets"][0]["rowSet"]
+    headers = {
+        'Host': 'stats.nba.com',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:61.0) Gecko/20100101 Firefox/61.0',
+        'Accept': 'application/json, text/plain, */*',
+        'Accept-Language': 'en-US,en;q=0.5',
+        'Referer': 'https://stats.nba.com/',
+        'Accept-Encoding': 'gzip, deflate, br',
+        'Connection': 'keep-alive',
+        'x-nba-stats-origin': 'stats',
+        'x-nba-stats-token': 'true'
+    }
+    gamelog = PlayerGameLog(player.id, headers=headers).get_dict()["resultSets"][0]["rowSet"]
     return Gamelog(entries=[gamelog_entry(game) for game in gamelog])
 
 
